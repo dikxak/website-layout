6f23ef7599e6f0e924f9e26170ca961502ae56f8
dikxak <dikshakpoudel76@gmail.com>
2022-09-11 23:06:31 +0545

    display active card component on left panel

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './components/Sidebar/Sidebar';
 
 import './app.css';
 import Card from './components/Card/Card';
 
 const App = () => {
+  const [activeComponent, setActiveComponent] = useState(
+    'No any active component'
+  );
+
+  const activeComponentChangeHandler = itemText => {
+    setActiveComponent(itemText);
+  };
+
   const sidebarData = [];
 
   for (let i = 1; i <= 20; i++) {
@@ -14,7 +22,8 @@ const App = () => {
   return (
     <section className="app">
       <main className="main-content">
-        <p>Main Content</p>
+        <p>Active Component:</p>
+        <p className="active-text">{activeComponent}</p>
       </main>
       <Sidebar
         render={item => {
@@ -22,6 +31,7 @@ const App = () => {
             <Card
               key={item}
               render={paraClass => <p className={paraClass}>{item}</p>}
+              onClick={activeComponentChangeHandler.bind(this, item)}
             ></Card>
           );
         }}
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,6 +5,7 @@ import classes from './Card.module.css';
 const Card = props => {
   return (
     <div
+      onClick={props.onClick}
       className={`${classes['card']} ${props.className ? props.className : ''}`}
     >
       {props.render(classes['card-para'])}
