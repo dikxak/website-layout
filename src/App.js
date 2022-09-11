1982330de3f45192753bd2eba0b23561f72396a4
dikxak <dikshakpoudel76@gmail.com>
2022-09-11 19:29:00 +0545

    add item to sidebarData dynamically

--- src/App.js
+++ src/App.js
@@ -4,28 +4,11 @@ import Sidebar from './components/Sidebar/Sidebar';
 import './app.css';
 
 const App = () => {
-  const sidebarData = [
-    'Item-1',
-    'Item-2',
-    'Item-3',
-    'Item-4',
-    'Item-5',
-    'Item-6',
-    'Item-7',
-    'Item-8',
-    'Item-9',
-    'Item-10',
-    'Item-11',
-    'Item-12',
-    'Item-13',
-    'Item-14',
-    'Item-15',
-    'Item-16',
-    'Item-17',
-    'Item-18',
-    'Item-19',
-    'Item-20',
-  ];
+  const sidebarData = [];
+
+  for (let i = 1; i <= 20; i++) {
+    sidebarData.push(`Item-${i}`);
+  }
 
   return (
     <section className="app">
