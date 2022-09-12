c087a55afe229bf140fec9d9069311f498cdb82b
dikxak <dikshakpoudel76@gmail.com>
2022-09-12 15:29:21 +0545

    add pagination for sidebar items

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ const App = () => {
 
   const sidebarData = [];
 
-  for (let i = 1; i <= 20; i++) {
+  for (let i = 1; i <= 100; i++) {
     sidebarData.push(`Item-${i}`);
   }
 
--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import classes from './Sidebar.module.css';
 
 const Sidebar = props => {
-  const initialListCount = 20;
+  const [currentPage, setCurrentPage] = useState(1);
+  const [indexRanges, setIndexRanges] = useState([]);
+
+  const totalItems = props.listData.length;
+  const itemPerPage = 20;
+  const totalPageCount = totalItems / itemPerPage;
+
+  const loadMoreBtnHandler = () => {
+    if (currentPage === totalPageCount) return;
+
+    setCurrentPage(prevPage => ++prevPage);
+  };
+
+  useEffect(() => {
+    const indexStart = (currentPage - 1) * itemPerPage; // For page 1: (1-1) * 20
+    const indexEnd = currentPage * itemPerPage; // For page 1: 1 * 20, range = (0-20)
+
+    setIndexRanges([indexStart, indexEnd]);
+  }, [currentPage]);
 
   return (
     <div className={classes['sidebar-container']}>
-      {props.listData.slice(0, initialListCount).map(item => {
+      {props.listData.slice(indexRanges[0], indexRanges[1]).map(item => {
         return props.render(item);
       })}
+      <button
+        onClick={loadMoreBtnHandler}
+        className={`btn ${classes['btn--load']}`}
+      >
+        Load More
+      </button>
     </div>
   );
 };
